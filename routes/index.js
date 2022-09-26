1fc8335dad500ea0219f5faf8d25aa083c85681b
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-09-26 11:19:55 -0700

    needs styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+let MovieModel = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+// MOVIES PAGE
+router.get('/movies', (req, res, next) => {
+    MovieModel.find()
+        .then((movie) => {
+          res.render('movies.hbs', {movie})
+      })
+      .catch((err) => {
+        console.log('Some error in finding', err)
+      })
+  })
+
+
+  router.get('/movies/:id', (req, res, next) => {
+    let {id} = req.params
+    MovieModel.findById(id)
+        .then((movie) => {
+          res.render('details.hbs', {movie})
+      })
+      .catch((err) => {
+        console.log('Some error in finding', err)
+      })
+  })
+
+
+
 
 module.exports = router;
