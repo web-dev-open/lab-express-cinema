0e685415a18e8b15992b60c0f63a055028201317
anshbindal <ansh.bindal1996@gmail.com>
2022-09-27 09:03:55 +0530

    Logic done, CSS left

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
+const MovieModel = require("../models/Movie.model");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  MovieModel.find()
+    .then((movies) => {
+      res.render("movies.hbs", { movies });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:id", (req, res, next) => {
+  const id = req.params.id;
+  MovieModel.findById(id)
+    .then((movie) => {
+      res.render("detail.hbs", { movie });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
