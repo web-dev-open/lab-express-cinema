771d93d0cdb23a0b25da2b090e079ecdaea4b444
james4-daf <d.james@elsevier.com>
2022-12-02 11:31:58 +0000

    all done

--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,19 @@ router.get("/movies", (req, res, next) => {
       console.log("Movies fetch failed", err);
     });
 });
+
+router.get("/movieInfo/:id", (req, res, next) => {
+  //make API call here
+  const { id } = req.params;
+  moviesModel
+    .findById(id)
+    .then((movie) => {
+      res.render("movieInfo.hbs", { movie });
+
+      console.log("Movie fetched", res);
+    })
+    .catch((err) => {
+      console.log("Movie fetch failed", err);
+    });
+});
 module.exports = router;
