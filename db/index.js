9780f29c4d597b648706ae163a982df99a233d68
parthpishte <parth.pishte21@vit.edu>
2024-02-04 12:29:03 +0530

    done

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
+require('dotenv/config');
+
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-cinema";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0/cinema" || "mongodb://localhost/cinema";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movie.model')
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    Movie
+        .find()
+        .then( movies => res.render('movies', {movies}))
+        .catch(err => console.log(err))
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+    const {movieId} = req.params 
+    Movie
+        .findById(movieId)
+        // .then( movie => res.send(movie))
+        .then( movie => res.render('movie-details', movie))
+        .catch(err => console.log(err))
+});
 
 module.exports = router;
